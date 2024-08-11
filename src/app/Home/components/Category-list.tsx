e5ef945e32fc522330731d25c414f38a55e3b4e3
Wander Amaral <wanderamaral013@gmail.com>
2024-08-11 14:52:51 -0300

    commit

---
+++ src/app/Home/components/Category-list.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@/components/Badge";
+import { View, Text } from "react-native";
+
+export default function CategoryList() {
+  return (
+    <View className="flex-row px-5 pt-5 pb-10  w-full justify-between">
+      <Badge
+        label="Trending Now"
+        variant={"secondary"}
+        labelClasses="text-white "
+      />
+      <Badge label="All" labelClasses="text-gray-500" />
+      <Badge label="New" labelClasses="text-gray-500"/>
+    </View>
+  );
+}
---
+++ src/app/Home/index.tsx
@@ -0,0 +1,83 @@
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import CategoryList from "./components/Category-list";
+
+interface ApiProduct {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface DataProducts {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+export default function Home() {
+  const [products, setProducts] = useState<DataProducts[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data: ApiProduct[] = await response.json();
+
+        const formattedData = data.map((item) => ({
+          id: item.id,
+          name: item.title,
+          price: item.price,
+          image: item.image,
+        }));
+        setProducts(formattedData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  const renderItem = ({ item }: { item: DataProducts }) => (
+    <View className="px-2 w-1/2">
+      <View className="rounded px-4 relative">
+        <Image
+          source={{ uri: item.image }}
+          className="w-full h-48 rounded-3xl"
+        />
+        <View className="absolute right-6 top-3 flex items-center rounded-full bg-white p-2">
+          <Ionicons name="heart-outline" size={24} color="#D91F1F" />
+        </View>
+        <Text className="text-lg font-bold mb-2">{item.name}</Text>
+        <Text className="text-base text-gray-600 ">
+          R$: {item.price.toFixed(2)}
+        </Text>
+      </View>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        ListHeaderComponent={
+          <View className="px-5 py-4">
+            <CategoryList />
+          </View>
+        }
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+}
