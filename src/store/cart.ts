739401e9e91ccd5df696ed5c8cc148d7a59859ea
Wander Amaral <wanderamaral013@gmail.com>
2024-08-21 14:18:27 -0300

    feat: add function remove product to cart, and add product if not in cart

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,16 +1,30 @@
 import { CartType } from "@/types/cart.types";
-import { Product } from "@/types/product.type";
 import { create } from "zustand";
 
 type CartStore = {
   products: CartType[];
   addProductToCart: (product: CartType) => void;
+  removeProductToCart: (id: string | number) => void;
 };
 
 export const useCartStore = create<CartStore>((set) => ({
   products: [],
   addProductToCart: (item) =>
+    set((state) => {
+      const productExists = state.products.find(
+        (product) => product.id === item.id
+      );
+
+      if (productExists) {
+        return state;
+      }
+
+      return {
+        products: [...state.products, { ...item, quantity: 1 }],
+      };
+    }),
+  removeProductToCart: (id) =>
     set((state) => ({
-      products: [...state.products, {...item, quantity: 1}],
+      products: state.products.filter((product) => product.id !== id),
     })),
 }));
