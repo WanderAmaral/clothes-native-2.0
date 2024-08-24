1318b471a2cba68b32dbb3a1cf29e459a683a005
Wander Amaral <wanderamaral013@gmail.com>
2024-08-24 13:09:36 -0300

    chore: add toast  and initalizing in component datails product

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
-import "@/styles/global.css";
-import Header from "@/components/Header";
-
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ActivityIndicator } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { useEffect } from "react";
-import { ActivityIndicator } from "react-native";
 import { tokenCache } from "@/storage/tokenCache";
-
 import Constants from "expo-constants";
 import { MyTabs } from "@/app/Screens";
+import { ToastProvider } from "@/components/Toast";
 
 const statusBarHeight = Constants.statusBarHeight;
 
@@ -35,24 +31,22 @@ function InitialLayout() {
 
 export default function App() {
   return (
-    <>
-      <ClerkProvider
-        publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}
-        tokenCache={tokenCache}
+    <ClerkProvider
+      publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <LinearGradient
+        colors={["#e5c3bf", "#dddddd", "#FFFFFF"]}
+        className="flex-1 shadow-lg" // Adicionando sombra com Tailwind
       >
-        <LinearGradient
-          colors={["#e5c3bf", "#dddddd", "#FFFFFF"]}
-          style={{ flex: 1 }}
-        >
-          <SafeAreaView style={{ flex: 1, marginTop: statusBarHeight + 8 }}>
+        <SafeAreaView style={{ flex: 1, marginTop: statusBarHeight + 8 }}>
+          <ToastProvider position="top">
             <InitialLayout />
             <StatusBar style="auto" />
-            
-
             <MyTabs />
-          </SafeAreaView>
-        </LinearGradient>
-      </ClerkProvider>
-    </>
+          </ToastProvider>
+        </SafeAreaView>
+      </LinearGradient>
+    </ClerkProvider>
   );
 }
--- src/app/Screens/DetailsPage/components/pageDetails.tsx
+++ src/app/Screens/DetailsPage/components/pageDetails.tsx
@@ -7,6 +7,7 @@ import Sizes from "../../../../components/Sizes";
 import { Button } from "@/components/Button";
 import { useCartStore } from "@/store/cart";
 import { useAuth, useOAuth } from "@clerk/clerk-expo";
+import { useToast } from "@/components/Toast";
 
 interface RouteParams {
   product: {
@@ -27,8 +28,7 @@ export default function DetailsPage({ navigation }: any) {
   const { product } = route.params;
   const { addProductToCart } = useCartStore();
   const { userId } = useAuth();
-
-  console.log(userId);
+  const { toast } = useToast();
 
   const googleOAuth = useOAuth({ strategy: "oauth_google" });
 
@@ -51,9 +51,8 @@ export default function DetailsPage({ navigation }: any) {
 
   const handleAddProduct = () => {
     if (!userId) {
-      onGoogleSignIn()
+      onGoogleSignIn();
     }
-
     const productToAdd = {
       id: product.id.toString(),
       name: product.name,
@@ -63,6 +62,13 @@ export default function DetailsPage({ navigation }: any) {
       quantity: 1,
     };
     addProductToCart(productToAdd);
+    toast(
+      `Product ${product.name} added to cart!`,
+      "success", // ou qualquer variante que você deseje
+      3000, // duração em milissegundos
+      "top", // posição do toast
+      true // mostrar barra de progresso
+    );
   };
 
   return (
